0da2209663b429936f7c4e7a15c7319dd3ec3488
Jesse Stuart <jessestuart@gmail.com>
2012-02-29 14:55:45 -0500

    Remove unnecessary actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,4 @@
 class PagesController < ApplicationController
-  # GET /pages
-  # GET /pages.json
-  def index
-    @pages = Page.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @pages }
-    end
-  end
-
   # GET /pages/1
   # GET /pages/1.json
   def show
@@ -32,11 +21,6 @@ class PagesController < ApplicationController
     end
   end
 
-  # GET /pages/1/edit
-  def edit
-    @page = Page.find(params[:id])
-  end
-
   # POST /pages
   # POST /pages.json
   def create
@@ -47,34 +31,6 @@ class PagesController < ApplicationController
     else
       flash[:error] = "Failed to fetch url! Please check the url and try again."
       redirect_to :action => :new
-    end
-  end
-
-  # PUT /pages/1
-  # PUT /pages/1.json
-  def update
-    @page = Page.find(params[:id])
-
-    respond_to do |format|
-      if @page.update_attributes(params[:page])
-        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /pages/1
-  # DELETE /pages/1.json
-  def destroy
-    @page = Page.find(params[:id])
-    @page.destroy
-
-    respond_to do |format|
-      format.html { redirect_to pages_url }
-      format.json { head :no_content }
     end
   end
 end
